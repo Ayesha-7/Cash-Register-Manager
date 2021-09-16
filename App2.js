315ac38ec3682f286e2e07d9f8495980bb082831
Ayesha <ayesha.sk707@gmail.com>
2021-09-16 17:55:07 +0530

    style: footer

--- App2.js
+++ App2.js
@@ -4,21 +4,22 @@ const nextButton = document.querySelector("#next-button");
 const checkButton = document.querySelector("#check-button");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 const nextMessage = document.querySelector("#next-message");
-const changeTable = document.querySelector(".change-table");
+const wrapTable = document.querySelector(".wrap-table");
 const checkMessage = document.querySelector("#check-message");
-const showOnNext = document.querySelector("#show-on-next");
+const wrapCashGiven = document.querySelector(".wrap-cash-given");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 //hiding cash input
-showOnNext.style.display = "none";
+wrapCashGiven.style.display = "none";
 checkMessage.style.display = "none";
+wrapTable.style.display = "none";
 
 nextButton.addEventListener("click", function validateBillAmount() {
     nextMessage.style.display = "none";
     if (billAmount.value > 0) {
         //if bill amount is valid given cash and other functionalities will be visible
         //bonus challenge
-        showOnNext.style.display = "block";
+        wrapCashGiven.style.display = "block";
     } else {
         showNextMessage("Bill amount should be greater than zero");
     }
@@ -28,24 +29,24 @@ nextButton.addEventListener("click", function validateBillAmount() {
 checkButton.addEventListener("click", function processing() {
     checkMessage.style.display = "none";
     nextMessage.style.display = "none";
-    setNotesZero();
+    wrapTable.style.display = "none";
+    //setNotesZero();
+    var cashval = Number(cashGiven.value);
+    var billval = Number(billAmount.value);
 
-    if (cashGiven.value <= 0) {
-        setNotesZero();
+    if (cashval <= 0) {
         showCheckMessage("Cash given cannot be negative or zero");
-    } else if (cashGiven.value === billAmount.value) {
-        setNotesZero();
+    } else if (cashval === billval) {
         showCheckMessage("Cash given is equal to bill amount. No need to return any cahnge");
-    } else if (cashGiven.value < billAmount.value) {
-        setNotesZero();
+    } else if (cashval < billval) {
         showCheckMessage("Cash given should be greater than bill amount");
     } else {
-        showCheckMessage("Return following no. of notes");
-        var dif = cashGiven.value - billAmount.value;
+        wrapTable.style.display = "block";
+        showCheckMessage("Return Change");
+        var dif = cashval - billval;
         calculateChange(dif);
     }
 })
-
 
 function showNextMessage(msg) {
     nextMessage.style.display = "block";
